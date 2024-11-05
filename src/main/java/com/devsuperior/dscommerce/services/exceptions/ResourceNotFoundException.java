be80098b16076cf5cb39a95f9df04e9bf6db0b3e
elcio2014 <elcio.gecont@gmail.com>
2024-11-05 01:08:05 -0300

    Validate self or admin

--- src/main/java/com/devsuperior/dscommerce/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/devsuperior/dscommerce/services/exceptions/ResourceNotFoundException.java
@@ -1,6 +1,5 @@
 package com.devsuperior.dscommerce.services.exceptions;
 
-@SuppressWarnings("serial")
 public class ResourceNotFoundException extends RuntimeException {
 
     public ResourceNotFoundException(String msg) {
